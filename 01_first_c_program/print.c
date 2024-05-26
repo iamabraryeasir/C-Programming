ff97778837cdadff67b3f47f329c5f9683f9c3fd
iamabraryeasir <162937581+iamabraryeasir@users.noreply.github.com>
2024-05-26 23:48:40 +0600

    Doing initial repo fixing.

---
+++ 01_first_c_program/print.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 20; // Age is a variable.
+
+    printf("Hi, I am Abrar.\n"); // \n is used to take something in new line.
+    printf("I am from Cumilla.\n");
+    printf("And I am %d years old.\n", age);
+
+    return 0;
+}
