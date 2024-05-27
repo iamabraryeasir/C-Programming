1f6fec2bf6e3e110a7b80311fb25b1fa08807ea6
abraryeasir-dev <abraryeasir.dev@gmail.com>
2024-05-27 19:56:23 +0600

    Chapter 01 Challenge 02 Completed.

---
+++ 01_first_c_program/printf_challenge_02.c
@@ -0,0 +1,49 @@
+/*
+Challenge-01: Print This patterns using just a single Printf.
+
+Pattern-01:
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+Pattern-02:
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+Pattern-03:
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Pattern-01:\n\n");
+
+    printf("*\n* *\n* * *\n* * * *\n* * * * *\n");
+
+    printf("\n\nPattern-02:\n\n");
+
+    printf("* * * * *\n* * * *\n* * *\n* *\n*\n");
+
+    printf("\n\nPattern-03:\n\n");
+
+    printf("* * * * *\n  * * * *\n    * * *\n      * *\n        *\n");
+
+    return 0;
+}
