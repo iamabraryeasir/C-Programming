0d211e92ca09965a7282b0d204e4fc66a2417d4f
iamabraryeasir <162937581+iamabraryeasir@users.noreply.github.com>
2024-05-27 19:49:24 +0600

    Cheapter 1 Challenge 1 completed.

---
+++ 01_first_c_program/printf_challenge.c
@@ -0,0 +1,61 @@
+/*
+Challenge-01: Print This patterns using just Printf.
+
+Pattern-01:
+
+*
+* *
+* * *
+* * * *
+* * * * *
+
+
+Pattern-02:
+
+* * * * *
+* * * *
+* * *
+* *
+*
+
+
+Pattern-03:
+
+* * * * *
+  * * * *
+    * * *
+      * *
+        *
+
+*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Pattern-01:\n\n");
+
+    printf("*\n");
+    printf("* *\n");
+    printf("* * *\n");
+    printf("* * * *\n");
+    printf("* * * * *\n");
+
+    printf("\n\nPattern-02:\n\n");
+
+    printf("* * * * *\n");
+    printf("* * * *\n");
+    printf("* * *\n");
+    printf("* *\n");
+    printf("*\n");
+
+    printf("\n\nPattern-03:\n\n");
+
+    printf("* * * * *\n");
+    printf("  * * * *\n");
+    printf("    * * *\n");
+    printf("      * *\n");
+    printf("        *\n");
+
+    return 0;
+}
