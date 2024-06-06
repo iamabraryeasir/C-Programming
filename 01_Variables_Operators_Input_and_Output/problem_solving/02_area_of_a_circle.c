fd555379578e79332751fcddb31fae4ca8bdf620
iamabraryeasir <162937581+iamabraryeasir@users.noreply.github.com>
2024-06-06 23:57:11 +0600

    Video 1 Complted

---
+++ 01_Variables_Operators_Input_and_Output/problem_solving/02_area_of_a_circle.c
@@ -0,0 +1,13 @@
+// Beecrowd 1002
+
+#include <stdio.h>
+
+int main()
+{
+    double R, PI = 3.14159;
+    scanf("%lf", &R);
+
+    double A = PI * R * R;
+    printf("A=%.4lf\n", A);
+    return 0;
+}
---
+++ 01_Variables_Operators_Input_and_Output/problem_solving/04_simple_product.c
@@ -0,0 +1,15 @@
+// Beecrowd 1004
+
+#include <stdio.h>
+
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    scanf("%d", &b);
+
+    int PROD = a * b;
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
